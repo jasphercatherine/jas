d9b4705b3f58b23c24ba289644d74ae0eabaa298
jasphercatherine <47554944+jasphercatherine@users.noreply.github.com>
2019-02-14 13:25:04 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,7 +1,9 @@
-jas=int(input())
-if jas%2==0:
-	print("Even")
-else:
-	print("Odd")
+jas=input()
+if jas.isnumeric():
+	g=int(jas)
+	if g%2==0:
+		print("Even")
+	else:
+		print("Odd")
 else:
 	print("invalid")
